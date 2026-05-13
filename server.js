80e72f7b7eb30ca57b975d2dc595a483676923a0
Dominion-hack <helenjoyikpe@gmail.com>
2026-05-13 22:33:42 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,59 +1,14 @@
 const express = require("express")
-const path = require("path")
-const fs = require("fs")
-
 const app = express()
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(express.static("public"))
 
-let config = {
-menuImage:
-"https://files.catbox.moe/jxw6jf.jpg"
-}
-
-app.get("/config", (req, res) => {
-res.json(config)
-})
-
-app.post("/update-image", (req, res) => {
-
-const { url } = req.body
-
-config.menuImage = url
-
-fs.writeFileSync(
-"./config.json",
-JSON.stringify(config, null, 2)
-)
-
-res.json({
-status: true,
-message: "Menu image updated"
-})
-
-})
-
-app.post("/restart", (req, res) => {
-
-res.json({
-status: true,
-message: "Bot restarting"
-})
-
-process.exit()
-
+app.get("/", (req, res) => {
+res.send("DOMINION BOT IS RUNNING ⚡")
 })
 
 app.listen(PORT, () => {
-console.log(`
-╔══════════════════════╗
-║ DOMINION SERVER ON ⚡
-╚══════════════════════╝
-
-Running:
-http://localhost:${PORT}
-`)
+console.log("Server running on port " + PORT)
 })
